05dfd80780e5fc7266c02013f453e0374acd507d
Shubham Yadav <luckyshubham16@gmail.com>
2022-09-13 20:49:54 +0530

    counter setup completed

--- app.js
+++ app.js
@@ -26,8 +26,13 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2023, 3, 24, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
 
+// let futureDate = new Date(2022, 8, 13, 20, 38, 0);
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
 const minutes = futureDate.getMinutes();
@@ -42,11 +47,48 @@ giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} ${hour
 
 // future time in ms
 const futureTime = futureDate.getTime();
-console.log(futureTime);
 
 function getRemainingTime() {
   const today = new Date().getTime();
-  console.log(today);
+  let t = futureTime - today;
+  // 1s = 1000ms
+  // 1m = 60s
+  // 1hr = 60min
+  // 1d = 24hr
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+  const oneSecond = 1000;
+
+  const days = Math.floor(t / oneDay);
+  t = t % oneDay;
+  const hours = Math.floor(t / oneHour);
+  t = t % oneHour;
+  const minutes = Math.floor(t / oneMinute);
+  t = t % oneMinute;
+  const seconds = Math.floor(t / oneSecond);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return `0${item}`;
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.textContent = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired</h4>`;
+  }
 }
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
 
 getRemainingTime();
