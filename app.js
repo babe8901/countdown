19e303af7b19263c3cdac462d55a60855efc1264
Shubham Yadav <luckyshubham16@gmail.com>
2022-09-13 10:57:58 +0530

    date setup completed

--- app.js
+++ app.js
@@ -21,3 +21,32 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2023, 3, 24, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const date = futureDate.getDate();
+
+let month = futureDate.getMonth();
+month = months[month];
+let day = futureDate.getDay();
+day = weekdays[day];
+
+giveaway.textContent = `giveaway ends on ${day}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  console.log(today);
+}
+
+getRemainingTime();
